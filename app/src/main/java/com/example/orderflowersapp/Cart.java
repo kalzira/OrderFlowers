c3a624eaa5b3df5d944ed973f9b459d7a50b076e
kalzira <kalzira.sabytakunova@iaau.edu.kg>
2019-12-20 08:55:47 +0600

    Cart, Order Status

---
+++ app/src/main/java/com/example/orderflowersapp/Cart.java
@@ -0,0 +1,142 @@
+package com.example.orderflowersapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.orderflowersapp.Common.Common;
+import com.example.orderflowersapp.Database.Database;
+import com.example.orderflowersapp.Model.Order;
+import com.example.orderflowersapp.Model.Request;
+import com.example.orderflowersapp.ViewHolder.CartAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Cart extends AppCompatActivity {
+
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference requests;
+
+    TextView txtTotalPrice;
+    Button btnPlaceOrder;
+
+    List<Order> cart = new ArrayList<>();
+    CartAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        //Init Firebase
+        database = FirebaseDatabase.getInstance();
+        requests = database.getReference("Requests");
+
+        //RecyclerView
+        recyclerView = findViewById(R.id.listCart);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        txtTotalPrice = findViewById(R.id.total);
+        btnPlaceOrder = findViewById(R.id.btnPlaceOrder);
+
+
+        //Open PopUp to fill shipping address
+        btnPlaceOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create new Request
+                showAlertDialog();
+
+            }
+        });
+
+        loadListFlower();
+
+
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Cart.this);
+        alertDialog.setTitle("Please fill information");
+        alertDialog.setMessage("Enter shipping address");
+        final EditText edtAddress = new EditText(Cart.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        edtAddress.setLayoutParams(lp);
+        //add edtText to alert dialog
+        alertDialog.setView(edtAddress);
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Create new Request
+                Request request = new Request(
+                        Common.currentUser.getPhone(),
+                        Common.currentUser.getName(),
+                        edtAddress.getText().toString(),
+                        txtTotalPrice.getText().toString(),
+                        cart
+                );
+
+                //Submit to Firebase
+                //We will be using System.CurentMilli to key
+                requests.child(String.valueOf(System.currentTimeMillis()))
+                        .setValue(request);
+
+                //Delete cart
+                new Database(getBaseContext()).cleanCart();
+                Toast.makeText(Cart.this, "Thank you, You will get flowers soon!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    private void loadListFlower() {
+        cart = new Database(this).getCarts();
+        adapter = new CartAdapter(cart, this);
+        recyclerView.setAdapter(adapter);
+
+        //Calculate Total Price
+        int total = 0;
+        for(Order order:cart)
+            total+=(Integer.parseInt(order.getPrice()))*(Integer.parseInt(order.getQuantity()));
+        Locale locale = new Locale("en","US");
+        NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+
+        txtTotalPrice.setText(fmt.format(total));
+    }
+}
---
+++ app/src/main/java/com/example/orderflowersapp/HomeAdmin.java
@@ -0,0 +1,158 @@
+package com.example.orderflowersapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.orderflowersapp.Interface.ItemClickListener;
+import com.example.orderflowersapp.Model.Category;
+import com.example.orderflowersapp.ViewHolder.MenuAdminViewHolder;
+import com.example.orderflowersapp.ViewHolder.MenuViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import static com.example.orderflowersapp.Common.Common.currentUser;
+
+public class HomeAdmin extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    FirebaseDatabase database;
+    DatabaseReference category;
+    TextView txtFullName;
+
+    RecyclerView recycler_menu;
+    RecyclerView.LayoutManager layoutManager;
+    FirebaseRecyclerAdapter<Category, MenuAdminViewHolder> adapter;
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_admin);
+
+        Toolbar toolbar = findViewById(R.id.toolbarAdmin);
+        toolbar.setTitle("Catalog Management");
+        setSupportActionBar(toolbar);
+
+
+        //Init Firebase
+        database = FirebaseDatabase.getInstance();
+        category = database.getReference("Category");
+
+        FloatingActionButton fab = findViewById(R.id.fabAdmin);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeAdmin.this, "fab clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_admin_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_admin);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Set name
+        View headerView = navigationView.getHeaderView(0);
+        txtFullName = headerView.findViewById(R.id.txtFullNameAdmin);
+        txtFullName.setText("Admin");
+
+        //Load menu
+        recycler_menu = findViewById(R.id.recycler_menuAdmin);
+        recycler_menu.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recycler_menu.setLayoutManager(layoutManager);
+
+        loadMenu();
+
+
+
+
+
+    }
+
+    private void loadMenu() {
+        adapter = new FirebaseRecyclerAdapter<Category, MenuAdminViewHolder>(Category.class,
+                R.layout.menu_admin_item,
+                MenuAdminViewHolder.class,
+                category) {
+            @Override
+            protected void populateViewHolder(MenuAdminViewHolder menuAdminViewHolder, Category category, int i) {
+                menuAdminViewHolder.txtMenuName.setText(category.getName());
+                Picasso.with(getBaseContext()).load(category.getImage())
+                        .into(menuAdminViewHolder.imageView);
+            }
+        };
+        adapter.notifyDataSetChanged(); //Refresh data when changed
+        recycler_menu.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_admin_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_catalog) {
+            // Handle the camera action
+        } else if (id == R.id.nav_cart) {
+
+
+        } else if (id == R.id.nav_log_out) {
+
+
+        } else if (id == R.id.nav_orders) {
+
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_admin_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
